48a36f9adcd4292ecf009f4eb27c22f94dd998be
baby7305 <songhaibei7305@163.com>
2020-12-01 17:54:24 +0800

    添加代码和图片

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     printf("hello xmake\n");
+    // Mat srcImage = imread("C:\\xmakeCode\\xmake-opencv-app//lianggongchunri_de_youyv-009.jpg");
+    Mat srcImage = imread("../../../../lianggongchunri_de_youyv-009.jpg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("[img]", srcImage);
     return a.exec();
 }
